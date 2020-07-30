a9ecd0c357b90dd781504a024dcc959a43a8f865
Pavleg <pajabonki@gmail.com>
2020-07-30 22:02:22 +0000

    update

--- main.py
+++ main.py
@@ -18,18 +18,38 @@ Heroes = {
     "DBB" : "Black Widow"
 }
 
-print("welcome to Marvel Heroes Quiz")
+print("welcome to Marvel Heroes Quiz\n")
 
-name = input("Hello what is your name? ")
-answer = input("Hello " + name + "! Ready to play? Y or N")
+name = input("Hello what is your name? \n")
+answer = input("Hello " + name + "! Ready to play? Y or N \n")
 print()
+
+
 if answer.upper() == "Y":
   print("Play game")
-  q1 = input("What color would you choose? (Choose A, B, C or D ) \nA) Blue \nB)Red \nC)Green \nD)Purple \n ")
+  ok = 0
+  while ok == 0:
+    q1 = input("What color would you choose? (Choose A, B, C or D ) \nA) Blue \nB)Red \nC)Green \nD)Purple \n ")
+    if q1.upper() in ("A", "B", "C", "D"):
+      ok = 1
+    else:
+      print("focus please! A, B, C or D")
+        
+  fine = 0
+  while fine == 0:
+    q2 = input("What sport do you prefer? (choose A or B) \nA) Soccer(Football) \nB) Tennis \n ")
+    if q2.upper() in ("A", "B"):
+      fine = 1
+    else:
+      print("pay attention to the question")
 
-  q2 = input("What sport do you prefer? (choose A or B) \nA) Soccer(Football) \nB) Tennis \n ")
-
-  q3 = input("What game do you prefer? (choose A or B) \nA) Mincraft \nB) Fortnite \n ")
+  well = 0
+  while well == 0:
+    q3 = input("What game do you prefer? (choose A or B) \nA) Mincraft \nB) Fortnite \n ")
+    if q3.upper() in ("A", "B"):
+      well = 1
+    else:
+      print("pick A or B")
 
   choice = q1 + q2 +q3
 
@@ -39,7 +59,4 @@ if answer.upper() == "Y":
   print("Your Marvel hero is " + Heroes[choice.upper()])
 
 if answer.upper() == "N":
-  print("if you want to find your marvel super hero you must press Y and enter")
-
-else:
-  print("you must press Y or N")
+  
